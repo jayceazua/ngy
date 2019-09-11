5f8a1e607578809399cecf15153c70a2031cb9d4
Richard Deacon <rich@yachtcloser.com>
2019-09-11 11:29:36 -0500

    Revisions for /home/closerweb/ngyachting.com - 2019.09.11-11.29.27

--- includes/ycconfig.php
+++ includes/ycconfig.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'closer_ngy');
 define('DB_PASSWORD', '@Bh96%hGR!nWT%Ut');
-define('DB_NAME', 'closer_ngy');
+define('DB_NAME', 'closer_ngyDEV');
 //end
 
 define('YCROOTPATH', $_SERVER['DOCUMENT_ROOT'] . "/");
