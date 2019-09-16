1f9763fa1216204776c62f67dd354095a99d37ce
Richard Deacon <rich@yachtcloser.com>
2019-09-16 10:34:39 -0500

    Fixed Dev YCconfig

--- includes/ycconfig.php
+++ includes/ycconfig.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'closer_ngy');
 define('DB_PASSWORD', '@Bh96%hGR!nWT%Ut');
-define('DB_NAME', 'closer_ngyDEV');
+define('DB_NAME', 'closer_ngy');
 //end
 
 define('YCROOTPATH', $_SERVER['DOCUMENT_ROOT'] . "/");
