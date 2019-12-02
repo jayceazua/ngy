db5474f8510ec422e32a0a34f99ea6dd91741efa
ec2-user <rich.deacon@boats.com>
2019-12-02 09:41:40 -0500

    Added in missing ycconfig file

---
+++ includes/ycconfig.php
@@ -0,0 +1,10 @@
+<?php
+//DB Connection Var
+define('DB_HOST', 'db.ngyachting.com');
+define('DB_USER', 'ngy');
+define('DB_PASSWORD', '@Bh96%hGR!nWT%Ut');
+define('DB_NAME', 'ngy');
+
+define('YCROOTPATH', $_SERVER['DOCUMENT_ROOT'] . "/");
+define('YCINCLUDEPATH', YCROOTPATH . "includes/");
+?>
