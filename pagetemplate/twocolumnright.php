459ad5a7f13ec5613d4b26e408e2a7c20dd9e2c0
Tanmay De <tanmay.de@boats.com>
2020-06-16 12:31:47 +0530

    KRAKEN-1869 - update 2

--- pagetemplate/twocolumnright.php
+++ pagetemplate/twocolumnright.php
@@ -7,24 +7,33 @@ $main_heading = "n";
 $breadcrumb = 1;
 $brdcmp_array = $frontend->create_bradcrumb_holder($pageid, $category_id_holder, $link_name);
 include($bdr."includes/head.php");
-?>
+
+$left_box_content = '
 <div class="leftcontentbox">
-<?php if ($connected_manufacturer_id == 0 AND $display_page_heading == 1){ ?>
-<h1 class="borderstyle1"><?php echo $frontend->head_title($link_name); ?></h1>
-<?php
+';
+
+if ($connected_manufacturer_id == 0 AND $display_page_heading == 1){
+	$left_box_content .= '<h1 class="borderstyle1">'. $frontend->head_title($link_name) .'</h1>';
 }else{
-	$_SESSION["s_normal_pagination"] = 2;	
+	$_SESSION["s_normal_pagination"] = 2;
 }
 
-if ($f_pdata != ""){ echo $f_pdata; }
-?>
-</div>
+if ($f_pdata != ""){
+	$left_box_content .= $f_pdata;
+}
 
-<div class="rightcontentbox scrollcol" parentdiv="main">
-<?php
-echo $frontend->display_box_content($column_id);
-?>
+$left_box_content .= '
 </div>
-<?php
+';
+
+$right_box_content = '
+<div class="rightcontentbox scrollcol" parentdiv="main">'. $frontend->display_box_content($column_id) .'</div>
+';
+
+if ($cm->isMobileDevice() AND $pageid == 165){
+	echo $right_box_content . $left_box_content;
+}else{
+	echo $left_box_content . $right_box_content;
+}
 include($bdr."includes/foot.php")
 ?>
