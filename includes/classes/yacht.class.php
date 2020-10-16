4f02402c815aa5366213817145ae1b5a58826536
Tanmay De <tanmay.de@boats.com>
2020-10-16 18:27:22 +0530

    update

--- includes/classes/yacht.class.php
+++ includes/classes/yacht.class.php
@@ -7731,7 +7731,7 @@ class Yachtclass {
 		}else{
 			$dcon = $cm->pagination_record;
 		}*/
-		$dcon = 20;
+		$dcon = 15;
         
         $page = ($p - 1) * $dcon;
         if ($page <= 0){ $page = 0; }
